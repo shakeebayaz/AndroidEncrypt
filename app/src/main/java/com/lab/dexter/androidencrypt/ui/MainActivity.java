847b6346e7cc29ebe0b9d9d653ebcc0150b53c4f
techjini <shakeeb@techjini.com>
2018-07-12 10:39:09 +0530

    refactored code

--- app/src/main/java/com/lab/dexter/androidencrypt/ui/MainActivity.java
+++ app/src/main/java/com/lab/dexter/androidencrypt/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.lab.dexter.androidencrypt;
+package com.lab.dexter.androidencrypt.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lab.dexter.androidencrypt.R;
 
 public class MainActivity extends AppCompatActivity {
 
